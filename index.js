3127237cd168d3fd728d3fa9e1bb681f13c9585a
anjana732 <anjanasingh1257@gmail.com>
2024-01-27 21:33:35 +0530

    Completed Color Matcher Project

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const bg = document.querySelector('body')
-const btn = document.querySelector('.button')
+const bg = document.querySelector("body");
+const btn = document.querySelectorAll('.button');
 
+
+btn.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            bg.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow'){
+            bg.style.backgroundColor = 'rgb(236, 236, 106)'
+        
+        }
+        if(e.target.id === 'white'){
+            bg.style.backgroundColor = e.target.id;
+           
+        }
+        if(e.target.id === 'red'){
+            bg.style.backgroundColor = 'rgb(241, 126, 126)';
+           
+        }
+    })  
+});
